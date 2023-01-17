b8b40671ac65aef7e6b800d04b6ebc6c5deb449e
Arnit Ibrahimi <arnitibra@gmail.com>
2023-01-17 18:02:52 +0000

    added outputs for private key, the private_key one especially is critical to make a file of your private key

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,19 @@ output "win_username" {
 output "public_ip" {
   value = azurerm_public_ip.public_ip.ip_address
 }
+
+output "private_key_sanity_check" {
+  value = tls_private_key.vm_ssh.id
+}
+
+output "private_key" {
+  value     = tls_private_key.vm_ssh.private_key_pem
+  sensitive = true
+}
+#terraform output -raw private_key > id_rsa
+
+#To get to the /etc directory for an installed distribution, use the distribution's command line with cd / to access the root directory, 
+#then ls to list files or explorer.exe . to view in Windows File Explorer. The directory path should look something like: /etc/wsl.conf.
+
+# using wsl ubuntu on vs code --> arnitibrahimi@DESKTOP-08H16SS:/mnt/c/ce04/project_mbappe$ chmod 400 id_rsa --changes
+#ssh -i id_rsa azureuser@<public_ip_address>
